4da71178597d48710c027252043a05df6ffb29f6
Jitesh Gosar <Jitesh17@users.noreply.github.com>
2019-06-24 16:47:57 +0900

    Add files via upload

--- scripts/visualise_scan.py
+++ scripts/visualise_scan.py
@@ -15,34 +15,42 @@ def distanceAt(angle, dr):
 
 
 def callback(data):
-    frame_size = 800
+    frame_size = 500
     mid_frame = int(frame_size/2)
     ymid_frame = mid_frame + 200
     frame = np.zeros((frame_size, frame_size, 3), np.uint8)
     angle = data.angle_min
     pi = data.angle_max - data.angle_min
     np.warnings.filterwarnings('ignore')
-    print(distanceAt(20, data.ranges))
+    # print(distanceAt(20, data.ranges))
     for r in data.ranges:
         assert isinstance(r, object)
 
         if r == np.inf:
             r = 10.
+        if r > 5:
+            r = 5.
         if np.isnan(r):
             r = 0.
         if angle == 15*pi/180:
             print(r)
-        x = math.trunc((r * 40) * math.cos(angle + (-90 * pi / 180)))
-        y = math.trunc((r * 40) * math.sin(angle + (-90 * pi / 180)))
-        x = -x
+        size = frame_size/22
+        x = math.trunc((r * 2 * size) * math.cos(angle + (-90 * pi / 180)))
+        y = math.trunc((r * 2 * size) * math.sin(angle + (-90 * pi / 180)))
+        # x = -x
         way_m = 2
         way_n = 20
+        cv2.line(frame, (x + mid_frame - 1, y + ymid_frame - 2), (x + mid_frame, y + ymid_frame), ((r)*255/5, (r)*255/5, (1-r/5)*255), 4)
+        cv2.line(frame, (mid_frame, ymid_frame), (x + mid_frame, y + ymid_frame), (55, 55, 55), 1)
         if -way_m*pi/180 < angle < way_m*pi/180:
-            cv2.line(frame, (mid_frame, ymid_frame), (x + mid_frame, y + ymid_frame), (0, 0, 255), 4)
+            # cv2.line(frame, (x + mid_frame - 1, y + ymid_frame-2), (x + mid_frame, y + ymid_frame), (20, 20, 255), 4)
+            cv2.line(frame, (mid_frame, ymid_frame), (x + mid_frame, y + ymid_frame), (44, 44, 77), 1)
         elif -way_n*pi/180 < angle < way_n*pi/180:
-            cv2.line(frame, (mid_frame, ymid_frame), (x + mid_frame, y + ymid_frame), (0, 255, 255), 4)
+            # cv2.line(frame, (x + mid_frame - 1, y + ymid_frame-2), (x + mid_frame, y + ymid_frame), (20, 255, 255), 4)
+            cv2.line(frame, (mid_frame, ymid_frame), (x + mid_frame, y + ymid_frame), (40, 70, 70), 1)
         else:
-            cv2.line(frame, (mid_frame, ymid_frame), (x + mid_frame, y + ymid_frame), (255, 0, 0), 2)
+            # cv2.line(frame, (x + mid_frame - 1, y + ymid_frame-2), (x + mid_frame, y + ymid_frame), (255, 20, 20), 4)
+            cv2.line(frame, (mid_frame, ymid_frame), (x + mid_frame, y + ymid_frame), (88, 55, 44), 1)
 
         angle = angle + data.angle_increment
 
